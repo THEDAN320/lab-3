bbd18b846f0960d60c1be395a549c06c0ea36ca4
THEDAN320 <103934625+THEDAN320@users.noreply.github.com>
2022-10-11 10:01:51 +0700

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -3,24 +3,36 @@
 
 int main() {
     using namespace std;
-    int predel,user_number = 0,all = 0,i = 0,maxint = 0;
+    int predel,user_number,all = 0,i = 0,maxint;
     FILE *f1, *f2;
     
     f1=fopen("predel.txt","r");
     fscanf(f1,"%d",&predel);
     fclose(f1);
     
-    while(all < predel){
+    do{
         cout<<"Введите число - "<<"\n";
         cin>>user_number;
         all += user_number;
+        cout<<all<<"\n";
         i++;
+        
+        if(i==1){
+            maxint = user_number;
+        }
+        
         if(maxint < user_number){
             maxint = user_number;
         }
+        
+        if(predel < all){
+            break;
+        }
+        
     
-    }
-    cout<<all;
+    }while(true);
+    
+    cout<<all<<"\n";
     f2 = fopen("kolmax.txt","w");
     fprintf(f2,"Количство введённых числ - %d, максимальное из них - %d",i,maxint);
     fclose(f2);
