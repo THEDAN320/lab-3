1eb2a60e7d1b1b6f14f9adefa0fec17d5a645d24
THEDAN320 <103934625+THEDAN320@users.noreply.github.com>
2022-11-08 20:06:33 +0700

    Update 2.cpp

--- 2/2.cpp
+++ 2/2.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <locale.h>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-double all(double n1, double n2, double n3) {
+double get_S(double n1, double n2, double n3) {
     double P, S;
     P = (n1 + n2 + n3) / 2;
     S = pow((P * (P - n1) * (P - n2) * (P - n3)), 0.5);
@@ -12,7 +13,7 @@ double all(double n1, double n2, double n3) {
     return S;
 }
 
-vector<double> get_v() {
+std::vector<double> get_v() {
     int number;
     vector<double> all;
     ifstream f1;
@@ -35,14 +36,13 @@ vector<double> get_v() {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    vector<int> all;
-
-    all = get_v();
-    int x = 0;
-
-    for (int i = x; i < 3; i++) {
-        all()
-        x++;
+    vector<double> all_n;
+    all_n = get_v();
+    double S = 0;
+    
+    for(int i = 0; i < all_n.size() - 2 ;i++){
+        S += get_S(all_n[i], all_n[i+1], all_n[i+2]);
+        cout<<endl;
     }
-
+    cout<<"S 12-ugolnika - "<<S;
 }
